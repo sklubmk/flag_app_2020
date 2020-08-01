1146de84fe14e6a2b478330d5ca4444127348e6b
sklubmk <sklubmk@gmail.com>
2020-08-01 22:58:56 +0900

    Updates

---
+++ precache-manifest.5812f061b129058962a871f9324b4364.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3cbd6f393de52a76c9309a1984591f9",
+    "url": "/flag_app_2020/index.html"
+  },
+  {
+    "revision": "c375ab40e1c71aec6312",
+    "url": "/flag_app_2020/static/css/main.e8719dcf.chunk.css"
+  },
+  {
+    "revision": "2a7e44b6b8ed26824f41",
+    "url": "/flag_app_2020/static/js/2.69940bfc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/flag_app_2020/static/js/2.69940bfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c375ab40e1c71aec6312",
+    "url": "/flag_app_2020/static/js/main.55538cf3.chunk.js"
+  },
+  {
+    "revision": "52d22e5d91470cd81e46",
+    "url": "/flag_app_2020/static/js/runtime-main.b1d74ee0.js"
+  }
+]);
